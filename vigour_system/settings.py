aa8db09092165d0701293647e4685dc9e22c9240
Ayush Tyagi <thetyagiayush@gmail.com>
2021-11-22 00:43:00 +0530

    Update settings.py

--- vigour_system/settings.py
+++ vigour_system/settings.py
@@ -66,7 +66,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vigour_system.wsgi.application'
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
